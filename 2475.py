58dc4ba58e70d53accd18cd0475209deb9258025
netsus <cwjcsk@naver.com>
2021-11-26 20:18:13 +0900

    2475_검증수 short coding solution

--- 2475.py
+++ 2475.py
@@ -10,3 +10,6 @@
 
 li=map(int,input().split()) # 5가지 숫자를 리스트형태로 받아서, li에 저장
 print(sum(map(lambda x: x**2,li))%10) # li에 각 인자를 제곱해서 더한 뒤, 10으로 나누기
+
+# 위의 풀이 숏코딩으로
+print(sum(map(lambda x: x**2,map(int,input().split())))%10)
